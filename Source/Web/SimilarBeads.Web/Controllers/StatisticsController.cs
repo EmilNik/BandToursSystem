d90cc5798a8455919c06f638b1d1d7b9fa042b2b
Emil Nik <emil.nik1002@gmail.com>
2016-02-21 16:38:04 +0200

    Added statistics controller

---
+++ Source/Web/SimilarBeads.Web/Controllers/StatisticsController.cs
@@ -0,0 +1,13 @@
+﻿namespace SimilarBeads.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class StatisticsController : BaseController
+    {
+        public ActionResult GetStatistics()
+        {
+            var statistics = new { };
+            return this.Json(new { Statistics = statistics });
+        }
+    }
+}
