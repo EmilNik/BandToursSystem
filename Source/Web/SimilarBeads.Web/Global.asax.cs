d9bad62b41d5fbf7cf70e09cf18bfbcf49048f64
Emil Nik <emil.nik1002@gmail.com>
2016-02-21 21:06:14 +0200

    Custom Errors

--- Source/Web/SimilarBeads.Web/Global.asax.cs
+++ Source/Web/SimilarBeads.Web/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿namespace SimilarBeads.Web
 {
+    using System;
     using System.Data.Entity;
     using System.Reflection;
     using System.Web;
@@ -7,11 +8,11 @@
     using System.Web.Optimization;
     using System.Web.Routing;
 
+    using Controllers;
     using Data;
     using Data.Migrations;
-
     using Infrastructure.Mapping;
-
+    
 #pragma warning disable SA1649 // File name must match first type name
     public class MvcApplication : HttpApplication
 #pragma warning restore SA1649 // File name must match first type name
@@ -30,6 +31,49 @@
 
             var autoMapperConfig = new AutoMapperConfig();
             autoMapperConfig.Execute(Assembly.GetExecutingAssembly());
+        }
+
+        protected void Application_Error()
+        {
+            if (this.Context.IsCustomErrorEnabled)
+            {
+                this.ShowCustomErrorPage(this.Server.GetLastError());
+            }
+        }
+
+        private void ShowCustomErrorPage(Exception exception)
+        {
+            var httpException = exception as HttpException ?? new HttpException(500, "Internal Server Error", exception);
+
+            this.Response.Clear();
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("fromAppErrorEvent", true);
+
+            switch (httpException.GetHttpCode())
+            {
+                case 403:
+                    routeData.Values.Add("action", "HttpError");
+                    break;
+
+                case 404:
+                    routeData.Values.Add("action", "HttpError");
+                    break;
+
+                case 500:
+                    routeData.Values.Add("action", "HttpError");
+                    break;
+
+                default:
+                    routeData.Values.Add("action", "GeneralError");
+                    routeData.Values.Add("httpStatusCode", httpException.GetHttpCode());
+                    break;
+            }
+
+            this.Server.ClearError();
+
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(this.Context), routeData));
         }
     }
 }
