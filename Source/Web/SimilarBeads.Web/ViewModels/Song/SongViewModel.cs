211d7af31272cdfef226221110c2b03b0a03fc6e
Emil Nik <emil.nik1002@gmail.com>
2016-02-23 19:31:18 +0200

    Updated Charts Views

--- Source/Web/SimilarBeads.Web/ViewModels/Song/SongViewModel.cs
+++ Source/Web/SimilarBeads.Web/ViewModels/Song/SongViewModel.cs
@@ -7,6 +7,8 @@
 
     public class SongViewModel : IMapFrom<Song>, IHaveCustomMappings
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public virtual ArtistViewModel Artist { get; set; }
